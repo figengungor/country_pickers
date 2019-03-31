bbe37e21b1906ce9fc2329eacadd0bedb0568046
endigo <tsogt.otgonbaatar@gmail.com>
2019-03-31 20:47:46 +0800

    add iso3Code field in model

--- lib/country.dart
+++ lib/country.dart
@@ -1,11 +1,13 @@
 class Country {
-  Country(this.isoCode, this.phoneCode, this.name);
+  Country(this.isoCode, this.iso3Code, this.phoneCode, this.name);
   final String name;
   final String isoCode;
+  final String iso3Code;
   final String phoneCode;
 
   Country.fromMap(Map<String, String> map)
       : name = map['name'],
         isoCode = map['isoCode'],
+        iso3Code = map['iso3Code'],
         phoneCode = map['phoneCode'];
 }
