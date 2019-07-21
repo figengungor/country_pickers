c4ee743e85d73a7dc72af2b31f07485a0a879522
Joshua Lorincz <joshlordeveloper@gmail.com>
2019-07-21 12:39:42 -0700

    Update country.dart

--- lib/country.dart
+++ lib/country.dart
@@ -2,13 +2,13 @@ class Country {
   final String name;
   final String isoCode;
   final String iso3Code;
-  final String phoneCode;
-  Country({this.isoCode, this.iso3Code, this.phoneCode, this.name});
+  final String currencyCode;
+  Country({this.isoCode, this.iso3Code, this.currencyCode, this.name});
 
   factory Country.fromMap(Map<String, String> map) => Country(
         name: map['name'],
         isoCode: map['isoCode'],
         iso3Code: map['iso3Code'],
-        phoneCode: map['phoneCode'],
+        phoneCode: map['currencyCode'],
       );
 }
