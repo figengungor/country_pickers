62fde8b20d172fb6e4e52137686273d35794cd4b
Joshua Lorincz <joshlordeveloper@gmail.com>
2019-07-21 19:17:18 -0700

    Update and rename country_pickers.dart to currency_pickers.dart

--- lib/country_pickers.dart
+++
@@ -1,6 +0,0 @@
-library country_pickers;
-
-export 'country_picker_dropdown.dart';
-export 'country_picker_dialog.dart';
-export 'country_picker_cupertino.dart';
-export 'utils/utils.dart';
---
+++ lib/currency_pickers.dart
@@ -0,0 +1,6 @@
+library country_pickers;
+
+export 'currency_picker_dropdown.dart';
+export 'currency_picker_dialog.dart';
+export 'currency_picker_cupertino.dart';
+export 'utils/utils.dart';
