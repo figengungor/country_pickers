3ddbac5464161e13c2b6962f910383efcbb0c9d9
Pawneshwer Gupta <pawneshwergupta@gmail.com>
2019-11-12 14:01:03 +0530

    added currency

--- lib/country.dart
+++ lib/country.dart
@@ -3,12 +3,24 @@ class Country {
   final String isoCode;
   final String iso3Code;
   final String phoneCode;
-  Country({this.isoCode, this.iso3Code, this.phoneCode, this.name});
+  final String currencyCode;
+  final String currencyName;
+
+  Country({
+    this.isoCode,
+    this.iso3Code,
+    this.phoneCode,
+    this.name,
+    this.currencyCode,
+    this.currencyName,
+  });
 
   factory Country.fromMap(Map<String, String> map) => Country(
         name: map['name'],
         isoCode: map['isoCode'],
         iso3Code: map['iso3Code'],
         phoneCode: map['phoneCode'],
+        currencyCode: map['currencyCode'],
+        currencyName: map['currencyName'],
       );
 }
