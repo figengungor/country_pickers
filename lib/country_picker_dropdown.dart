dc02a19d1f9a68595c9eaa2f4b03f6dea62266c8
Pradeep KN <pradeep.kn@liftoffllc.com>
2020-03-09 01:54:28 +0530

    Update country_picker_dropdown.dart
    Updated default value to false.

--- lib/country_picker_dropdown.dart
+++ lib/country_picker_dropdown.dart
@@ -11,7 +11,7 @@ class CountryPickerDropdown extends StatefulWidget {
     this.itemBuilder,
     this.initialValue,
     this.onValuePicked,
-    this.isExpanded
+    this.isExpanded = false,
   });
 
   /// Filters the available country list
