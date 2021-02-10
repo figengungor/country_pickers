324c7cd142975b4191be602f484a9d5002ef0dd6
Hemant Kaushik <thehemantkaushik@gmail.com>
2021-02-10 12:45:02 +0530

    Update my_alert_dialog.dart

--- lib/utils/my_alert_dialog.dart
+++ lib/utils/my_alert_dialog.dart
@@ -136,7 +136,7 @@ class MyAlertDialog<T> extends StatelessWidget {
 
     if (actions != null) {
       if (isDividerEnabled) children.add(divider);
-      children.add(new ButtonTheme.bar(
+      children.add(ButtonTheme.fromButtonThemeData(
         child: new ButtonBar(
           children: actions,
         ),
