cdb1e12bfe7164fc4a495582677fabb17e914669
omarhurani <omarhurani@gmail.com>
2021-03-07 15:13:28 +0200

    Fix ButtonTheme.bar removed

--- lib/utils/my_alert_dialog.dart
+++ lib/utils/my_alert_dialog.dart
@@ -136,7 +136,8 @@ class MyAlertDialog<T> extends StatelessWidget {
 
     if (actions != null) {
       if (isDividerEnabled) children.add(divider);
-      children.add(new ButtonTheme.bar(
+      children.add(new ButtonBarTheme(
+        data: ButtonBarThemeData(),
         child: new ButtonBar(
           children: actions,
         ),
